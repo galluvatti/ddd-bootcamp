96e1935b6b6c9d7f2bbf4551ecab24041bbd0bf2
galluvatti <galluvatti@gmail.com>
2024-03-19 11:55:55 +0100

    implemented DELETE aircraft by id

--- fleet-management/src/routes/aircrafts.ts
+++ fleet-management/src/routes/aircrafts.ts
@@ -1,7 +1,5 @@
 import {Request, Response, Router} from 'express';
 
-import {Aircraft} from "../types/aircraft";
-
 const router = Router();
 
 const Pool = require('pg').Pool
@@ -13,8 +11,6 @@ const pool = new Pool({
     port: 5432,
 })
 
-// Add your CRUD API implementation here
-
 router.get('/', (req: Request, res: Response) => {
     pool.query('SELECT * FROM fleetops.AIRCRAFTS ORDER BY id ASC', (error: any, results: { rows: any; }) => {
         if (error) {
@@ -25,12 +21,24 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 router.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
-    pool.query('INSERT INTO fleetops.AIRCRAFTS (data) VALUES ($1) RETURNING *', [req.body], (error: any, results: { rows: { id: any; }[]; }) => {
+    pool.query('INSERT INTO fleetops.AIRCRAFTS (data) VALUES ($1) RETURNING *', [req.body], (error: any, results: {
+        rows: { id: any; }[];
+    }) => {
         if (error) {
             throw error
         }
         res.status(201).send(`Aircraft added with ID: ${results.rows[0].id}`)
+    })
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    const aircraftID = req.params.id;
+
+    pool.query('DELETE FROM fleetops.AIRCRAFTS WHERE id = $1', [aircraftID], (error: any, results: any) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).send(`Aircraft deleted with ID: ${aircraftID}`)
     })
 });
 
